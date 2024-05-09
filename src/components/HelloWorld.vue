e639aacd9205f14f6cd9c21428d4278fb6ce297f
deanWinnie <524635843@qq.com>
2024-05-09 21:06:45 +0800

    数据部分编写完成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,18 +6,10 @@
   >
     <el-table-column
       prop="date"
-      label="日期"
-      width="180">
+      label="名称"
+      >
     </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址">
-    </el-table-column>
+    
   </el-table>
  </div>
 </template>
---
+++ src/until/dataInfo.js
@@ -0,0 +1,37 @@
+export const dataForCss = [
+  '对 BFC 的理解',
+  '盒模型介绍',
+  '实现两栏布局',
+  '经典三分栏布局',
+  '水平垂直居中',
+  'flex: 1代表什么意思',
+  '重排（reflow）和重绘（repaint）的理解'
+]
+export const dataForJs = [
+  '基本的数据类型介绍，及值类型和引用类型的理解',
+  '数据类型的判断',
+  '手写深拷贝',
+  '原型和原型链',
+  '作用域与作用域链',
+  '闭包',
+  '手写bind',
+  'new 实现',
+  'event loop',
+  'Promise',
+  'async/await 和 Promise 的关系'
+]
+export const dataForHTML = [
+  'script 标签中 defer 和 async ',
+  'HTML 语义化',
+  '从浏览器地址栏输入 url 到请求返回发生了什么',
+  'http 状态码',
+  'web 存储',
+  'http 缓存'
+]
+export const dataForhands = [
+  '防抖',
+  '节流',
+  '数组扁平化',
+  '去重',
+
+]
