6293215e7b50df32c808e8ddd98b9a00e7302954
deanWinnie <524635843@qq.com>
2024-05-09 17:07:26 +0800

    创建项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+ <div>
+  <el-button>获取随机10道题目</el-button>
+  <el-table
+    style="width: 100%"
+  >
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+  </el-table>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
